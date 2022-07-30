76aff86a6eb7a7e67e7cddedd5641c349d2571f5
Hanif <hanif.hakim@spesolution.com>
2022-07-30 20:42:17 +0700

    Change Site Name

--- src/apis/firebase.js
+++ src/apis/firebase.js
@@ -1,18 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
+const apiKey = process.env.REACT_APP_FIREBASE_API_KEY;
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAg2HJTK2FDDrytjmLlUWZeIIYUyGIEZmo",
+  apiKey: apiKey,
   authDomain: "valorant-point.firebaseapp.com",
   projectId: "valorant-point",
   storageBucket: "valorant-point.appspot.com",
   messagingSenderId: "20907655786",
-  appId: "1:20907655786:web:40710ba5b0f2b647074013"
+  appId: "1:20907655786:web:40710ba5b0f2b647074013",
 };
 
 // Initialize Firebase
